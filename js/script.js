1ebe542529eeeaae9c239e5bc4cb28f0294b62bc
Shafiq <Shahramfarokhi421@gmail.com>
2024-11-18 16:08:54 +0430

    Slider section added

--- js/script.js
+++ js/script.js
@@ -206,6 +206,51 @@ function dispalyBackgroundImage(type, background_path) {
   }
 }
 
+// Display Slider movies
+
+ async function displaySlider() {
+  const {results} = await fetchAPIData('movie/now_playing');
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+    ` ;
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwipper();
+  } )
+}
+
+function initSwipper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerview: 1, 
+    spaceBetween : 30,
+    freemode: true,
+    loop: true,
+    autoPlay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2
+      },
+      700: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
+  })
+}
+
 //  Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
 
@@ -253,6 +298,7 @@ function init() {
     switch (global.currentPage) {
         case '/':
             case '/index.html':
+              displaySlider();
             displayPopularMovies();           
             break;
         case '/shows.html':
